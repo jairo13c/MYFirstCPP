a0538188bff7736f6e30c97ddf17a5275bd9c3d7
jairo13c <31705891+jairo13c@users.noreply.github.com>
2018-01-23 12:46:01 -0700

    added class file

---
+++ MyFirstCPP/Controller/controller.cpp
@@ -0,0 +1,10 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Cortez, Jairo on 1/23/18.
+//  Copyright © 2018 Cortez, Jairo. All rights reserved.
+//
+
+#include "Controller.hpp"
+
---
+++ MyFirstCPP/Controller/controller.hpp
@@ -0,0 +1,20 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Cortez, Jairo on 1/23/18.
+//  Copyright © 2018 Cortez, Jairo. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
