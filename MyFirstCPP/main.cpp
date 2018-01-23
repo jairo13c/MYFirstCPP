8e6a6d35b0bcf8e8528e18dd3386a0467ff89a71
jairo13c <31705891+jairo13c@users.noreply.github.com>
2018-01-23 12:31:52 -0700

    creating y first C++ proyecct

---
+++ MyFirstCPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Cortez, Jairo on 1/23/18.
+//  Copyright © 2018 Cortez, Jairo. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!\n"<< endl;
+    string input;
+    getline(cin, input);
+    cout << "you said" << input << endl;
+    return 100;
+}
